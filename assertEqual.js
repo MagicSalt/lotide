e5fbda8a4043b39d6c46fe69bd503041e635c869
Darren Lin <darrenlin815@hotmail.com>
2022-10-17 17:37:56 -0700

    Cleaned up function code

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
+const assertEqual = (actual, expected) => {
   if (actual === expected) {
-    console.log('👍👍👍Assertion Passed: ' + actual + ' === ' + expected);
-  } else console.log('👎👎👎Assertion Failed: ' + actual + ' !== ' + expected);
+    console.log(`👍👍👍Assertion Passed: ${actual} === ${expected}`);
+  } else console.log(`👎👎👎Assertion Failed: ${actual} !== ${expected}`);
 };
 
 //TEST CODE
