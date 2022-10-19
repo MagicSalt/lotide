6666fc58152bfc37adcfed46d0897503b8481247
Darren Lin <darrenlin815@hotmail.com>
2022-10-19 12:35:43 -0700

    Reformatting

--- assertEqual.js
+++ assertEqual.js
@@ -2,10 +2,12 @@
 const assertEqual = (actual, expected) => {
   if (actual === expected) {
     console.log(`👍👍👍Assertion Passed: ${actual} === ${expected}`);
-  } else console.log(`👎👎👎Assertion Failed: ${actual} !== ${expected}`);
+  } else { 
+    console.log(`👎👎👎Assertion Failed: ${actual} !== ${expected}`);
+  }
 };
 
-//TEST CODE
+// TEST CODE
 assertEqual('Lighthouse Labs', 'Bootcamp');
 assertEqual(1, 1);
 assertEqual('Darren', 'Darren');
