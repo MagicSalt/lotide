5f63e882269df92fb76e69722a3c9ef77caba8e2
Darren Lin <darrenlin815@hotmail.com>
2022-10-24 16:09:55 -0700

    Refactoring

--- tail.js
+++ tail.js
@@ -1,22 +1,7 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = (actual, expected) => {
-  if (actual === expected) {
-    console.log(`👍👍👍Assertion Passed: ${actual} === ${expected}`);
-  } else console.log(`👎👎👎Assertion Failed: ${actual} !== ${expected}`);
-};
+// ACTUAL FUNCTION
 
-const tail = (array) => {
+const tail = function(array) {
   return array.slice(1);
 };
 
-// TEST CODE
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-
-const word = ['Darren'];
-console.log(tail(word));
-
-const empty = [];
-console.log(tail(empty));
+module.exports = tail;
