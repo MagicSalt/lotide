a2929538996ebf51b0fe8b5c9663e458db448e0a
Darren Lin <darrenlin815@hotmail.com>
2022-10-24 16:01:02 -0700

    Refactor

--- head.js
+++ head.js
@@ -1,15 +1,9 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = (actual, expected) => {
-  if (actual === expected) {
-    console.log(`👍👍👍Assertion Passed: ${actual} === ${expected}`);
-  } else console.log(`👎👎👎Assertion Failed: ${actual} !== ${expected}`);
-};
+// assertEqual FUNCTION
+const assertEqual = require('./assertEqual');
 
-const head = (array) => {
+// ACTUAL FUNCTION
+const head = function(array) {
   return (array[0]);
 };
 
-// TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), 5);
+module.exports = head;
