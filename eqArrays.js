2c5a6d9c99ad3063057cb704450ab498a80036e1
Darren Lin <darrenlin815@hotmail.com>
2022-10-24 16:16:19 -0700

    Refactoring

--- eqArrays.js
+++ eqArrays.js
@@ -1,16 +1,20 @@
-const eqArrays = function (actual, expected) {
+// ACTUAL FUNCTION
+
+const eqArrays = function(actual, expected) {
   if (actual.length !== expected.length) {
     return false;
-  } else {
-    for (let i = 0; i < actual.length; i++) {
-      if (Array.isArray(actual[i]) && Array.isArray(expected[i])) {
-        if (!eqArrays(actual[i], expected[i])) {
-          return false;
-        }
-      } else if (actual[i] !== expected[i]) {
+  }
+
+  for (let i = 0; i < actual.length; i++) {
+    if (Array.isArray(actual[i]) && Array.isArray(expected[i])) {
+      if (!eqArrays(actual[i], expected[i])) {
         return false;
       }
+    } else if (actual[i] !== expected[i]) {
+      return false;
     }
-    return true;
   }
-}
+  return true;
+};
+
+module.exports = eqArrays;
