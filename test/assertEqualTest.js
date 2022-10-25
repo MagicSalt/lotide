f5726c9cbcf59a84a2cadf86613e4c5322f8da77
Darren Lin <darrenlin815@hotmail.com>
2022-10-24 18:18:47 -0700

    Refactored for use with M&R

---
+++ test/assertEqualTest.js
@@ -0,0 +1,10 @@
+// assertEqual FUNCTION
+
+const assertEqual = require('../assertEqual');
+
+// TEST CODE FOR assertEqual.js
+
+assertEqual('Lighthouse Labs', 'Bootcamp');
+assertEqual(1, 1);
+assertEqual('Darren', 'Darren');
+assertEqual(2, 5);
